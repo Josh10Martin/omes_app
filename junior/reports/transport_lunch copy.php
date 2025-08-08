3c9ec1cb9dd39ee0d34c4dc5e843367fdc04c70b
kaula clement <kaulaclement@gmail.com>
2025-08-08 10:37:30 +0200

    updated lunch and transport

--- junior/reports/transport_lunch copy.php
+++ junior/reports/transport_lunch copy.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+error_reporting(E_ALL);
+ini_set('error_log', __DIR__ . '/pdf_errors.log');
 session_start();
 require_once '../../dompdf/autoload.inc.php';
 include '../../config.php';
